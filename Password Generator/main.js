eae767ffca527a8c1394efe4b140fc333ad20c4c
Dusan <zajecarweb@gmail.com>
2024-10-27 13:47:38 +0100

    Update main.js

--- Password Generator/main.js
+++ Password Generator/main.js
@@ -3,16 +3,67 @@ let Lower = document.getElementById("Lower");
 let Numberr = document.getElementById("Number");
 let Symbol = document.getElementById("Symbol");
 
-function clicked(x) {
+let PassDisplay = document.getElementsByClassName("password")
+let Button = document.querySelector("button");
+
+let UpperState = false;
+let LowerState = false;
+let NumberrState = false;
+let SymbolState = false;
+
+let Uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let Lowers = "abcdefghijklmnopqestuvwxyz";
+let Numberrs = "0123456789";
+let Symbols = "!$%_?-";
+
+let PassLength = 10;
+let Options = 0;
+let Password = "";
+
+Button.addEventListener("click", () => {
+    
+    Password = "";
+    Options = -1;
+
+    if (UpperState) Options = 0;
+    if (LowerState) Options += 1;
+    if (NumberrState) Options += 1;
+    if (SymbolState) Options += 1;
+
+    if (Options != -1) {
+
+        for (let i = 0; i < PassLength; i++) {
+
+            let choise = Math.floor(Math.random() * Options);
+            if (choise == 0) Password += Uppers.charAt(Math.floor(Math.random() * Uppers.length()));
+            if (choise == 1) Password += Lowers.charAt(Math.floor(Math.random() * Lowers.length()));
+            if (choise == 2) Password += Numberrs.charAt(Math.floor(Math.random() * Numberrs.length()));
+            if (choise == 3) Password += Symbols.charAt(Math.floor(Math.random() * Symbols.length()));
+
+        }
+    } else {
+        Password = "Invalid Choise!";
+    }
+    PassDisplay.innerHTML = Password;
+    console.log(Password, UpperState, LowerState, NumberrState, SymbolState);
+})
+
+Upper.addEventListener("click", () => clicked(Upper, UpperState));
+Lower.addEventListener("click", () => LowerState = clicked(Lower, LowerState));
+Numberr.addEventListener("click", () => NumberrState = clicked(Numberr, NumberrState));
+Symbol.addEventListener("click", () => SymbolState = clicked(Symbol, SymbolState));
+
+function clicked(x, y) {
+
     if (x.className == "unchecked") {
         x.className = "checked";
+        y = true;
+        console.log(y);
+        return y;
     } else {
         x.className = "unchecked";
+        y = false;
+        console.log(y);
+        return y;
     }
-    console.log("Clicked " + x);
 }
-
-Upper.addEventListener("click", () => clicked(Upper));
-Lower.addEventListener("click", () => clicked(Lower));
-Numberr.addEventListener("click", () => clicked(Numberr));
-Symbol.addEventListener("click", () => clicked(Symbol));
